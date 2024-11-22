19600303eb4efa514764ee6a32eaf3190ed6da24
Luis Rojo <montesrojol84@gmail.com>
2024-11-22 12:29:20 -0600

    Login and register

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -4,4 +4,14 @@ homeController.getHome = async (req, res) => {
     res.render('../src/views/home.ejs');
 };
 
+homeController.login = async (req, res) => {
+    res.render('../src/views/login.ejs');
+};
+homeController.register = async (req, res) => {
+    res.render('../src/views/register.ejs');
+};
+
+
+
+
 export default homeController;
