cc9c0f8c0de4f1b33a15896671a8d2fd596f6360
Ana Lucía Ramírez <Luca San Juan>
2024-11-30 18:38:18 -0600

    merge

--- src/database.js
+++ src/database.js
@@ -4,6 +4,6 @@ import {createPool} from 'mysql2/promise';
 export const pool  = createPool({
     host: 'localHost',
     user: 'root',
-    password: 'CodyJunior2',
+    password: 'CodyJunior2.',
     database: 'panaderiapan'
 })
