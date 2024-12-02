e1499ded4dc00511aa663fbf5c445ed55bd09d7b
SherlockTM <d21390008@huauchinango.tecnm.mx>
2024-12-02 12:16:40 -0600

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,16 +1,19 @@
-import productController from "../controllers/producto.controller.js"
-import {Router} from "express";
+//import productoController from "../controllers/producto.controller.js";
+import { getAllProducts, getOneProduct, insertProduct, updateProduct, deleteProduct, getAllProductsforCard 
+} from '../controllers/producto.controller.js';
+import express from "express";
 
-const router = Router()
+const router = express.Router();
 
-router.post('/insertProduct',productController.insertProduct);
-router.get('/getOneProduct/:id_product', productController.getOneProduct);
-router.get('/getAllProducts', productController.getAllProducts);
-router.put('/updateOne/:id_product', productController.UpdateOneProduct);
-router.delete('/deleteOne/:id_product', productController.DeleteOneProduct);
+// Rutas de productos
+router.get('/inventario', getAllProducts);  // Verifica que esta función esté definida en el controlador
+router.get('/getOne/:id_product', getOneProduct);  // Verifica que esta función esté definida en el controlador
+router.post('/insert', insertProduct);  // Verifica que esta función esté definida en el controlador
+router.post('/update/:id_product', updateProduct);  // Verifica que esta función esté definida en el controlador
+router.get('/deleteOne/:id_product', deleteProduct);  // Asegúrate de que esta función también esté bien definida
 
-//Rutas para las tarjetas 
-router.get('/getOneProductforCard/:id_product', productController.getOneProductforCard);
-router.get('/getAllProductsforCard', productController.getAllProductsforCard);
+// Rutas para las tarjetas
+//router.get('/getOneProductforCard/:id_product', getOneProductforCard);  // Verifica que esta función esté definida
+router.get('/getAllProductsforCard', getAllProductsforCard);  // Verifica que esta función esté definida
 
 export default router;
